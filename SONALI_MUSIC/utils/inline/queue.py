bd16392651fb17a2e60362ac5c68a2806c6c9ab2
huu680222-oss <huu680222@gmail.com>
2026-07-21 20:34:10 +0530

    Update queue.py

--- SONALI_MUSIC/utils/inline/queue.py
+++ SONALI_MUSIC/utils/inline/queue.py
@@ -75,10 +75,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="• ᴊσɪη ησω •", url=f"https://t.me/SpicyxNetwork"
+                text="• ᴊσɪη ησω •", url=f"https://t.me/your_fairytale07"
             ),
             InlineKeyboardButton(
-                text="• ɢʀᴏᴜᴘ ᴄʜᴀᴛ •", url=f"https://t.me/+gXCu09qmgwA0NjA9"
+                text="• ɢʀᴏᴜᴘ ᴄʜᴀᴛ •", url=f"https://t.me/Xbroze"
             ),
         ],
       ]
