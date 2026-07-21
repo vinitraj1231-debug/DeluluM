df37b660cc52d1919c17add1b8335dbefd1ce436
huu680222-oss <huu680222@gmail.com>
2026-07-21 20:38:47 +0530

    Update __main__.py

--- SONALI_MUSIC/__main__.py
+++ SONALI_MUSIC/__main__.py
@@ -50,12 +50,12 @@ async def init():
         pass
     await Sona.decorators()
     LOGGER("SONALI_MUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗔𝗟𝗣𝗛𝗔☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 RAJ☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("SONALI_MUSIC").info("𝗦𝗧𝗢𝗣 𝗦𝗢𝗡𝗔𝗟𝗜 𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧..")
+    LOGGER("SONALI_MUSIC").info("𝗦𝗧𝗢𝗣 RAJ 𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
